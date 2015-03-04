c7f9b7576a0ad782ee684a11401d1c47af83f359
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-03 20:47:13 -0500

    Begin OOP for editor and page

--- riddle.js
+++ riddle.js
@@ -1,3 +1,18 @@
-function Riddle() {
-  
+// Riddle constructor
+// Riddle objects represent the entire editor
+// parameters:
+// DOMElement container - container for the editor
+
+function Riddle(container) {
+  this.container = container;
+}
+
+// Page constructor
+// GUI element for a page in a document
+// parameters:
+// Riddle editor - Riddle object for the editor itself
+
+function Page(editor) {
+  this.container = document.createElement("div");
+  editor.container.appendChild(this.container);
 }
